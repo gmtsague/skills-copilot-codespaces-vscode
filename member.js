7ab01ea6110faca3462d2127ef8d3bf1d506c31d
gmtsague <115669552+gmtsague@users.noreply.github.com>
2023-08-02 15:11:51 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+  var skills = {
+    name: 'John Doe',
+    age: 30,
+    address: '123 Main St',
+    skills: ['HTML', 'CSS', 'JS'],
+    getSkills: function() {
+      return this.skills;
+    }
+  };
+  // return skills;
+  return skills.getSkills();
+}
